286cffffcd78975e188776ec28262e7b41d0cb48
nskulyberdin <nikitskul2012@gmail.com>
2023-05-14 15:28:05 +0300

    Init

---
+++ financial-flow-control-service/src/main/java/ru/nskul/cadms/financialflowcontrolservice/FinancialFlowControlServiceApplication.java
@@ -0,0 +1,13 @@
+package ru.nskul.cadms.financialflowcontrolservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinancialFlowControlServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinancialFlowControlServiceApplication.class, args);
+	}
+
+}
---
+++ financial-flow-control-service/src/test/java/ru/nskul/cadms/financialflowcontrolservice/FinancialFlowControlServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.nskul.cadms.financialflowcontrolservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinancialFlowControlServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
